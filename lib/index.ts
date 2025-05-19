1c4c3e0c58af86085915e68d0cef39935cec6733
Andreas Herd <mendrik76@gmai.com>
2025-05-19 16:56:59 +0300

    chore: update package version to 0.0.3 and fix exports in index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,7 @@
 import { awaitObj } from './await-obj'
 import { evolveAlt } from './evolve-alt'
 import { failOn } from './guards'
-import { listToTree } from './list-to-tree'
+import { listToTree, type TreeOf } from './list-to-tree'
 import { mapBy } from './map-by'
 import { caseOf, match } from './match'
 import { pipeAsync } from './pipe-async'
@@ -10,13 +10,14 @@ import { traverse } from './traverse'
 
 export {
 	awaitObj,
+	caseOf,
 	evolveAlt,
 	failOn,
 	listToTree,
 	mapBy,
 	match,
-	caseOf,
 	pipeAsync,
 	pipeTap,
-	traverse
+	traverse,
+	type TreeOf
 }
