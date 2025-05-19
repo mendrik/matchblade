54720dfe3b68d7555a12c3c23e05a9f6b8e1f217
Andreas Herd <mendrik76@gmai.com>
2025-05-19 17:07:25 +0300

    refactor: change failOn to a function declaration and update package version to 0.0.4

--- lib/guards.ts
+++ lib/guards.ts
@@ -1,7 +1,12 @@
-export const failOn = <T, G extends T>(
+export function failOn<T, G extends T>(
 	guard: (x: T) => x is G,
 	message: string
-) => {
+): (x: T) => Exclude<T, G>
+
+export function failOn<T, G extends T>(
+	guard: (x: T) => x is G,
+	message: string
+) {
 	return (x: T): Exclude<T, G> => {
 		if (guard(x)) {
 			throw new Error(message)
